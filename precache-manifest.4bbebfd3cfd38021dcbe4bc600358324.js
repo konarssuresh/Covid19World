779f328b2504493e0fb19122c86b3d0c6115afe6
Suresh Konar <SKonar@chicago.local>
2020-06-02 16:18:21 +0530

    Updates

---
+++ precache-manifest.4bbebfd3cfd38021dcbe4bc600358324.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17f0ec753e2024d0fbec91c4088fdbe",
+    "url": "/Covid19World/index.html"
+  },
+  {
+    "revision": "c01715d12253d211de28",
+    "url": "/Covid19World/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1dcc2819b0d2c8c0bcc8",
+    "url": "/Covid19World/static/css/main.f6dab2af.chunk.css"
+  },
+  {
+    "revision": "c01715d12253d211de28",
+    "url": "/Covid19World/static/js/2.e6c95fa2.chunk.js"
+  },
+  {
+    "revision": "ed6d44160be3da7ea632241f05f7ba30",
+    "url": "/Covid19World/static/js/2.e6c95fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dcc2819b0d2c8c0bcc8",
+    "url": "/Covid19World/static/js/main.cb58a569.chunk.js"
+  },
+  {
+    "revision": "9b31ee43b81768b40d9a",
+    "url": "/Covid19World/static/js/runtime-main.e87d4152.js"
+  },
+  {
+    "revision": "c5af19dd23e54c093ff726d30edb63c4",
+    "url": "/Covid19World/static/media/covid-icon.c5af19dd.ico"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Covid19World/static/media/logo.5d5d9eef.svg"
+  }
+]);
