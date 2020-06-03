ac0bb13c3fe62bcae3878784e65f22fa0e7ccc7e
Suresh Konar <SKonar@chicago.local>
2020-06-03 11:12:21 +0530

    Ui issue due to link length issue fixed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {NavLink,withRouter} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const footer=props=>(
-    <footer class="page-footer font-small bg-dark text-white-50 pt-4">
-  <div class="container-fluid text-center text-md-left">
-    <div class="row">
-      <div class="col-md-6 mt-md-0 mt-3">
-        <h5 class="text-uppercase">DECLARATION</h5>
-        <p class="text-justify">The counts has been taken as per the  website of https://github.com/konarssuresh/Covid19World/tree/master and 
+    <footer className="page-footer font-small bg-dark text-white-50 pt-4">
+  <div className="container-fluid text-center text-md-left">
+    <div className="row">
+      <div className="col-md-6 mt-md-0 mt-3">
+        <h5 className="text-uppercase">DECLARATION</h5>
+        <p className="text-justify">The counts has been taken as per the  website of https://corona-api.com/ and 
           is  updated time to time as the data is updated in the site.</p>
       </div>
-      <hr class="clearfix w-100 d-md-none pb-3"/>
-      <div class="col-md-3 mb-md-0 mb-3">
-        <h5 class="text-uppercase">Links</h5>
-        <ul class="list-unstyled">
+      <hr className="clearfix w-100 d-md-none pb-3"/>
+      <div className="col-md-3 mb-md-0 mb-3">
+        <h5 className="text-uppercase">Links</h5>
+        <ul className="list-unstyled">
           <li>
             <NavLink to="/dashboard" >Dashboard</NavLink >
           </li>
@@ -24,7 +24,7 @@ const footer=props=>(
       </div>
     </div>
   </div>
-  <div class="footer-copyright text-center py-3">© 2020:
+  <div className="footer-copyright text-center py-3">© 2020:
     <a href="https://github.com/konarssuresh/Covid19World"> Covid19World</a>
   </div>
 </footer>
